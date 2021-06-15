286b778dd9a0e34345c2fd871effacfc1059b829
Amgd2112 <amgad.hussein99.com>
2021-06-15 02:53:18 +0200

    fix image orientation, check extension

--- database/database_helper.py
+++ database/database_helper.py
@@ -1,14 +1,14 @@
-import face_recognition
-from fastapi import UploadFile, File
-from numpy import asarray
-from PIL import Image
+import os
 from io import BytesIO
 
+import face_recognition
+from fastapi.datastructures import UploadFile
+from numpy import asarray
+from PIL import ExifTags, Image
 
-def check_extension(file: UploadFile):
-    extension = file.filename.split('.')[-1].lower() in ('jpg', 'jpeg', 'png')
-    if not extension:
-        return 'Image must be jpg or png format!'
+
+def is_allowed_extension(name: str) -> bool:
+    return os.path.splitext(name.lower())[1] in ('.jpg', '.jpeg', '.png')
 
 
 def has_face(image: Image.Image) -> bool:
@@ -21,12 +21,42 @@ def has_face(image: Image.Image) -> bool:
     return True
 
 
-async def read_image_file(file: File):
-    myfile = await file.read()
-    image_file = BytesIO(myfile)
-    image = Image.open(image_file)
-    if has_face(image=image):
-        # check if there is any face exists in the image
-        return image
+async def read_image_file(file: UploadFile) -> Image.Image:
+    # check if file is an image with jpg, jpeg, png
+    if is_allowed_extension(name=file.filename):
+        myfile = await file.read()
+        image_file = BytesIO(myfile)
+        image = fix_orientation(image=Image.open(image_file))
+        image.filename = file.filename
 
-    raise Exception('No face detected.')
+        # check if there is any face exists in the image
+        if has_face(image=image):
+            return image
+
+        raise Exception('No face detected!')
+
+    raise Exception('Image must be jpg, jpeg or png format!')
+
+
+def fix_orientation(image: Image.Image) -> Image.Image:
+    THUMB_WIDTH, THUMB_HIGHT = 1000, 1000
+    if hasattr(image, '_getexif'):
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+
+        e = image._getexif()
+        if e is not None:
+            exif = dict(e.items())
+            orientation = exif[orientation]
+
+            if orientation == 3:
+                image = image.transpose(Image.ROTATE_180)
+            elif orientation == 6:
+                image = image.transpose(Image.ROTATE_270)
+            elif orientation == 8:
+                image = image.transpose(Image.ROTATE_90)
+
+        image.thumbnail((THUMB_WIDTH, THUMB_HIGHT), Image.ANTIALIAS)
+
+    return image
